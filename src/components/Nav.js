93feeb80714013de9972cd69324bb41718e7070a
Kittipan Khanthong <kittipankhanthong@Kittipans-MacBook-Air-2.local>
2023-01-29 20:24:54 +0700

    menuNav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,14 @@ import SearchIcon from "@mui/icons-material/Search";
 import LanguageIcon from "@mui/icons-material/Language";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
 
 const Nav = () => {
     return (
@@ -29,10 +36,96 @@ const Nav = () => {
                     ให้เช่าที่พักกับ Airbnb
                 </p>
                 <LanguageIcon className="h-6 cursor-pointer" />
-                <div className="flex items-center space-x-2 border p-2 rounded-full hover:shadow-md duration-100 ease-out cursor-pointer">
-                    <MenuIcon className="h-6" />
-                    <AccountCircleIcon className="h-6" />
-                </div>
+                {/* button menu */}
+                <Menu as="div" className="relative inline-block text-left">
+                    <div>
+                        <Menu.Button className="flex items-center space-x-2 border p-2 rounded-full hover:shadow-md duration-100 ease-out cursor-pointer">
+                            <MenuIcon className="" />
+                            <AccountCircleIcon className="" />
+                        </Menu.Button>
+                    </div>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            ลงทะเบียน
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            เข้าสู่ระบบ
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                            <div className="py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            ให้เช่าที่พักกับ Airbnb
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            จัดประสบการณ์
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                'block px-4 py-2 text-sm'
+                                            )}
+                                        >
+                                            ช่วยเหลือ
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
             </div>
         </div>
     )
